4feab206e18db99e3b942cc4f3f7ea3323377685
Chandru6303 <chandruvasan65@gmail.com>
2025-08-25 13:49:15 +0530

    Moved to controller file

--- app.js
+++ app.js
@@ -1,61 +1,13 @@
 const express = require('express');
+const controller = require('./controller')
 const app = express();
-let data = [];
 app.use(express.json());
 
-app.post('/insert', insertdata);
-app.get('/getAllStudents', (req, res) => {
-    console.log("[INFO] Entered into Get All Students");
-    res.send(data);
-});
-app.get('/getStudentByRollNo', getStudentByRollNo);
-app.delete('/deleteStudent', deleteStudent);
+app.post('/insert', controller.insertdata);
+app.get('/getAllStudents', controller.getAllStudents);
+app.get('/getStudentByRollNo', controller.getStudentByRollNo);
+app.delete('/deleteStudent', controller.deleteStudent);
 
 
-function getStudentByRollNo(req, res) {
-    console.log("[INFO] Entered into Get Student By Roll No");
-    const rollNo = req.body.rollNo;
-    const student = data.find(student => student.rollNo === rollNo);
-    if (student) {
-        console.log("[SUCCESS] Student Found");
-        res.status(200).send(student);
-    } else {
-        console.log("[ERROR] Student Not Found");
-        res.status(404).send("Student Not Found");
-    }
-}
 
-function insertdata(req, res) {
-    console.log("[INFO] Entered into Insert Data");
-    let isDuplicate = checkIfDataIsPresent(req.body.rollNo);
-    if(!isDuplicate) {
-        console.log("[INFO] No Duplicate Found");
-        data.push(req.body);
-        console.log("[SUCCESS] Data Inserted Successfully");
-        res.send('Data Inserted');
-    }
-    else {
-        console.log("[INFO] Duplicate Record Found");
-        res.send("Record Already Exists");
-    }
-}
-function  checkIfDataIsPresent(rollNo) {
-    for(let i of data) {
-        if(i.rollNo === rollNo) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function deleteStudent(req, res) {
-    let rollNo = req.body.rollNo;
-    let index = data.findIndex(s => s.rollNo === rollNo);
-    if (index !== -1) {
-        data.splice(index, 1);
-        res.send("Student Deleted");
-    } else {
-        res.status(404).send("Student Not Found");
-    }
-}
 app.listen(3000);
